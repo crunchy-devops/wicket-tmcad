5ecf71db97a15aa98ce0faf231d2a1311588a593
Herve <dockerlite@gmail.com>
2025-04-01 15:31:09 +0200

    add project.dxf as junit test

--- src/test/java/com/crunchydevops/dxf/DxfReaderTest.java
+++ src/test/java/com/crunchydevops/dxf/DxfReaderTest.java
@@ -15,6 +15,15 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("DxfReader Tests")
 class DxfReaderTest {
+    private static final String PROJECT_DXF = "data/project.dxf";
+    private static final String LAYER_ZERO = "0";
+    private static final String LAYER_CARROYAGE = "carroyage";
+    private static final String LAYER_CROIX = "croix";
+    private static final String LAYER_TERRAIN = "terrain naturel";
+    private static final String LAYER_Z_VALUE = "z value TN";
+    private static final String LAYER_CLOTURE = "cloture";
+    private static final String LAYER_CONSTRUCTION = "$CONSTRUCTION";
+    
     @TempDir
     Path tempDir;
     
@@ -22,6 +31,51 @@ class DxfReaderTest {
         Path dxfFile = tempDir.resolve(filename);
         Files.writeString(dxfFile, content);
         return dxfFile;
+    }
+    
+    @Test
+    @DisplayName("Project DXF file should be correctly read")
+    void testProjectDxf() throws IOException {
+        DxfReader reader = new DxfReader(Path.of(PROJECT_DXF));
+        Map<String, DxfLayer> layers = reader.readLayers();
+        
+        // Verify all layers exist
+        assertAll("Project layers existence",
+            () -> assertTrue(layers.containsKey(LAYER_ZERO), "Layer 0 should exist"),
+            () -> assertTrue(layers.containsKey(LAYER_CARROYAGE), "Layer carroyage should exist"),
+            () -> assertTrue(layers.containsKey(LAYER_CROIX), "Layer croix should exist"),
+            () -> assertTrue(layers.containsKey(LAYER_TERRAIN), "Layer terrain naturel should exist"),
+            () -> assertTrue(layers.containsKey(LAYER_Z_VALUE), "Layer z value TN should exist"),
+            () -> assertTrue(layers.containsKey(LAYER_CLOTURE), "Layer cloture should exist"),
+            () -> assertTrue(layers.containsKey(LAYER_CONSTRUCTION), "Layer $CONSTRUCTION should exist")
+        );
+        
+        // Verify layer 0 (default layer)
+        DxfLayer layer0 = layers.get(LAYER_ZERO);
+        assertAll("Layer 0 properties",
+            () -> assertEquals(LAYER_ZERO, layer0.name(), "Layer name should be 0"),
+            () -> assertEquals(-7, layer0.colorNumber(), "Color should be -7"),
+            () -> assertEquals("Continuous", layer0.lineType(), "Line type should be Continuous"),
+            () -> assertFalse(layer0.isVisible(), "Layer should be invisible")
+        );
+        
+        // Verify z value TN layer
+        DxfLayer layerZValue = layers.get(LAYER_Z_VALUE);
+        assertAll("z value TN layer properties",
+            () -> assertEquals(LAYER_Z_VALUE, layerZValue.name(), "Layer name should be z value TN"),
+            () -> assertEquals(-18, layerZValue.colorNumber(), "Color should be -18"),
+            () -> assertEquals("Continuous", layerZValue.lineType(), "Line type should be Continuous"),
+            () -> assertFalse(layerZValue.isVisible(), "Layer should be invisible")
+        );
+        
+        // Verify croix layer (colored layer)
+        DxfLayer layerCroix = layers.get(LAYER_CROIX);
+        assertAll("croix layer properties",
+            () -> assertEquals(LAYER_CROIX, layerCroix.name(), "Layer name should be croix"),
+            () -> assertEquals(-147, layerCroix.colorNumber(), "Color should be -147"),
+            () -> assertEquals("Continuous", layerCroix.lineType(), "Line type should be Continuous"),
+            () -> assertFalse(layerCroix.isVisible(), "Layer should be invisible")
+        );
     }
     
     @Nested
@@ -84,224 +138,6 @@ class DxfReaderTest {
             
             assertThrows(IOException.class, () -> new DxfReader(nonexistentFile),
                         "Should throw IOException for non-existent file");
-        }
-    }
-    
-    @Nested
-    @DisplayName("Layer Tests")
-    class LayerTests {
-        private static final String LAYER_NAME = "z_value_TN";
-        private static final String HIDDEN_LAYER = "HIDDEN_LAYER";
-        
-        @Test
-        @DisplayName("Layer with TEXT entity should be correctly read")
-        void testLayerWithText() throws IOException {
-            String dxfContent = """
-                0
-                SECTION
-                2
-                HEADER
-                0
-                ENDSEC
-                0
-                SECTION
-                2
-                TABLES
-                0
-                TABLE
-                2
-                LAYER
-                0
-                LAYER
-                2
-                %s
-                70
-                0
-                62
-                7
-                6
-                CONTINUOUS
-                0
-                ENDTAB
-                0
-                ENDSEC
-                0
-                SECTION
-                2
-                ENTITIES
-                0
-                TEXT
-                8
-                %s
-                10
-                100.0
-                20
-                200.0
-                1
-                300.0
-                0
-                ENDSEC
-                0
-                EOF
-                """.formatted(LAYER_NAME, LAYER_NAME);
-            
-            Path dxfFile = createDxfFile("text.dxf", dxfContent);
-            DxfReader reader = new DxfReader(dxfFile);
-            Map<String, DxfLayer> layers = reader.readLayers();
-            
-            assertTrue(layers.containsKey(LAYER_NAME), "Layer should exist");
-            DxfLayer layer = layers.get(LAYER_NAME);
-            
-            // Verify layer properties
-            assertAll("Layer properties",
-                () -> assertEquals(LAYER_NAME, layer.name(), "Layer name should match"),
-                () -> assertEquals(7, layer.colorNumber(), "Color number should be 7"),
-                () -> assertEquals("CONTINUOUS", layer.lineType(), "Line type should be CONTINUOUS"),
-                () -> assertTrue(layer.isVisible(), "Layer should be visible")
-            );
-            
-            // Verify TEXT entity
-            assertAll("TEXT entity",
-                () -> assertEquals(1, layer.entities().size(), "Should have one entity"),
-                () -> assertEquals("TEXT", layer.entities().get(0).type(), "Entity type should be TEXT"),
-                () -> assertEquals("100.0", layer.entities().get(0).groupCodes().get(10), "X coordinate"),
-                () -> assertEquals("200.0", layer.entities().get(0).groupCodes().get(20), "Y coordinate"),
-                () -> assertEquals("300.0", layer.entities().get(0).groupCodes().get(1), "Text content")
-            );
-        }
-
-        @Test
-        @DisplayName("Invisible layer should have negative color and be marked invisible")
-        void testInvisibleLayer() throws IOException {
-            String dxfContent = """
-                0
-                SECTION
-                2
-                HEADER
-                0
-                ENDSEC
-                0
-                SECTION
-                2
-                TABLES
-                0
-                TABLE
-                2
-                LAYER
-                0
-                LAYER
-                2
-                %s
-                70
-                0
-                62
-                -7
-                6
-                CONTINUOUS
-                0
-                ENDTAB
-                0
-                ENDSEC
-                0
-                SECTION
-                2
-                ENTITIES
-                0
-                ENDSEC
-                0
-                EOF
-                """.formatted(HIDDEN_LAYER);
-            
-            Path dxfFile = createDxfFile("invisible.dxf", dxfContent);
-            DxfReader reader = new DxfReader(dxfFile);
-            Map<String, DxfLayer> layers = reader.readLayers();
-            
-            assertTrue(layers.containsKey(HIDDEN_LAYER), "Hidden layer should exist");
-            DxfLayer layer = layers.get(HIDDEN_LAYER);
-            
-            assertAll("Hidden layer properties",
-                () -> assertEquals(HIDDEN_LAYER, layer.name(), "Layer name should match"),
-                () -> assertEquals(-7, layer.colorNumber(), "Color should be -7"),
-                () -> assertEquals("CONTINUOUS", layer.lineType(), "Line type should be CONTINUOUS"),
-                () -> assertFalse(layer.isVisible(), "Layer should be invisible")
-            );
-        }
-
-        @Test
-        @DisplayName("Multiple layers should be correctly read")
-        void testMultipleLayers() throws IOException {
-            String dxfContent = """
-                0
-                SECTION
-                2
-                HEADER
-                0
-                ENDSEC
-                0
-                SECTION
-                2
-                TABLES
-                0
-                TABLE
-                2
-                LAYER
-                0
-                LAYER
-                2
-                %s
-                70
-                0
-                62
-                7
-                6
-                CONTINUOUS
-                0
-                LAYER
-                2
-                %s
-                70
-                0
-                62
-                -7
-                6
-                CONTINUOUS
-                0
-                ENDTAB
-                0
-                ENDSEC
-                0
-                SECTION
-                2
-                ENTITIES
-                0
-                ENDSEC
-                0
-                EOF
-                """.formatted(LAYER_NAME, HIDDEN_LAYER);
-            
-            Path dxfFile = createDxfFile("multiple.dxf", dxfContent);
-            DxfReader reader = new DxfReader(dxfFile);
-            Map<String, DxfLayer> layers = reader.readLayers();
-            
-            assertEquals(2, layers.size(), "Should have two layers");
-            assertTrue(layers.containsKey(LAYER_NAME), "First layer should exist");
-            assertTrue(layers.containsKey(HIDDEN_LAYER), "Second layer should exist");
-            
-            // Verify first layer
-            DxfLayer layer1 = layers.get(LAYER_NAME);
-            assertAll("First layer properties",
-                () -> assertEquals(LAYER_NAME, layer1.name()),
-                () -> assertEquals(7, layer1.colorNumber()),
-                () -> assertTrue(layer1.isVisible())
-            );
-            
-            // Verify second layer
-            DxfLayer layer2 = layers.get(HIDDEN_LAYER);
-            assertAll("Second layer properties",
-                () -> assertEquals(HIDDEN_LAYER, layer2.name()),
-                () -> assertEquals(-7, layer2.colorNumber()),
-                () -> assertFalse(layer2.isVisible())
-            );
         }
     }
 }
